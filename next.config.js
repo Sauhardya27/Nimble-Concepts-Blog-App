78310d4ad927ae1cfcce400c276206e245c1552c
Sauhardya27 <sauhardyachakraborty2643@gmail.com>
2024-10-04 21:20:33 +0530

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -1,22 +1,24 @@
+const path = require('path');
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	webpack: (config) => {
-		config.resolve.alias = {
-			...config.resolve.alias,
-			'@': path.resolve(__dirname, './'),
-		};
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname, './'),
+    };
 
-		return config;
-	},
-	images: {
-		domains: ['avatars.githubusercontent.com'],
-		remotePatterns: [
-			{
-				protocol: 'https',
-				hostname: "images.pexels.com"
-			}
-		]
-	}
+    return config;
+  },
+  images: {
+    domains: ['avatars.githubusercontent.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: "images.pexels.com"
+      }
+    ]
+  }
 }
 
 module.exports = nextConfig
